064f3a6e3f23718d05132ec86e7eb81f4c0a0155
Vladislav Treshchev <letsgovlad@gmail.com>
2024-08-26 12:00:58 +0300

    Just 1 function

---
+++ Instructions/Instructions.cpp
@@ -0,0 +1,19 @@
+// Program Instructions
+// Describes work with functions
+
+#include <iostream>
+
+// function prototype
+
+void Instructions();
+
+int main()
+{
+	Instructions();
+}
+
+void Instructions()
+{
+	std::cout << "Welcome to the most fun you've ever had with text!\n\n";
+	std::cout << "Here's how to play the game....\n";
+}
